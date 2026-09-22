623e28a55df3f632934999f47ce0da68aef13f1a
ziadashery15-ux <ziadashery15@gmail.com>
2026-09-22 19:22:51 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='ziad-v6-1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./images/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
